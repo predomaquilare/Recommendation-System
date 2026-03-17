b2b76875c1c0cd84f1f28a6b1e6e7e5355440ddf
William Torres <contatowilliamtorres@gmail.com>
2026-03-16 21:32:55 -0300

    refactor: clean_vector function and vectorToMap removed. Every client/product are attributed on their respective list on the first iteration.

--- purchase history module/include/purchase_history_modulelib.hpp
+++ purchase history module/include/purchase_history_modulelib.hpp
@@ -24,8 +24,6 @@ class PurchaseHistory {
     std::unordered_map<int,int> get_items_from_client(int client_id); 
 
   private:
-    std::vector<std::string> all_clients;
-    std::vector<std::string> all_products;
     std::vector<std::list<int>> purchase_history;
     std::vector<std::vector<std::string>> hash_vector;
     std::unordered_map<std::string, int> map_client_to_id;
@@ -34,8 +32,6 @@ class PurchaseHistory {
     std::unordered_map<int, std::string> map_id_to_product;
     bool quit_flag;
 
-    void vectorToMap(std::vector<std::string> &vetor, std::unordered_map<std::string, int> &mapping_to_id, std::unordered_map<int, std::string> &id_to_mapping); 
-    void clean_vector(std::vector<std::string> &clean_vector);
     void load_csv(int num, char **file);
     void terminal_access();
 };
--- purchase history module/src/purchase_history_modulelib.cpp
+++ purchase history module/src/purchase_history_modulelib.cpp
@@ -9,30 +9,7 @@ PurchaseHistory::PurchaseHistory(int num, char **file, bool terminal) {
 PurchaseHistory::~PurchaseHistory(){}
 
 void PurchaseHistory::load_csv(int num, char **file) {
-  for (int i = 1; i <= num; i++) {
-    std::ifstream arquivo(file[i]);
-    std::string linha;
-    if (!arquivo.is_open()) {
-      std::cerr << "Erro ao abrir arquivo: " << file[i] << std::endl;
-      continue;
-    }
-    while (std::getline(arquivo, linha)) {
-      std::stringstream ss(linha);
-      std::string data, cliente, produto, nome;
-      std::getline(ss, data, ',');
-      if(data == "DATA_COMPRA") continue;
-      std::getline(ss, cliente, ',');
-      std::getline(ss, produto, ',');
-      std::getline(ss, nome);
-      all_clients.push_back(cliente);
-      all_products.push_back(nome);
-    }
-  }
-  PurchaseHistory::clean_vector(all_clients);
-  PurchaseHistory::clean_vector(all_products);
-  PurchaseHistory::vectorToMap(all_clients, map_client_to_id, map_id_to_client);
-  PurchaseHistory::vectorToMap(all_products, map_product_to_id, map_id_to_product);
-  purchase_history = std::vector<std::list<int>>(all_clients.size());
+  unsigned long int client_count = 0, product_count = 0;
 
   for (int i = 1; i <= num; i++) {
     std::ifstream arquivo(file[i]);
@@ -49,21 +26,24 @@ void PurchaseHistory::load_csv(int num, char **file) {
       std::getline(ss, cliente, ',');
       std::getline(ss, produto, ',');
       std::getline(ss, nome);
-      purchase_history[map_client_to_id[cliente]].push_back(map_product_to_id[nome]);
+      if (map_client_to_id.find(cliente) == map_client_to_id.end()){
+        map_client_to_id[cliente] = client_count;
+        map_id_to_client[client_count] = cliente;
+        purchase_history.push_back(std::list<int>());
+        client_count++;
+      }
+      if (map_product_to_id.find(nome) == map_product_to_id.end()) {
+        map_product_to_id[nome] = product_count;
+        map_id_to_product[product_count] = nome;
+        product_count++;
+      }
+      int client_id = map_client_to_id[cliente];
+      int product_id = map_product_to_id[nome];
+      purchase_history[client_id].push_back(product_id);
     }
   }
-}
-
-void PurchaseHistory::vectorToMap(std::vector<std::string> &vetor, std::unordered_map<std::string, int> &mapping_to_id, std::unordered_map<int, std::string> &id_to_mapping) {
-  for(size_t i = 0; i < vetor.size(); i++) {
-    mapping_to_id[vetor[i]] = i;
-    id_to_mapping[i] = vetor[i];
-  }
-}
-
-void PurchaseHistory::clean_vector(std::vector<std::string> &clean_vector) {
-  std::sort(clean_vector.begin(), clean_vector.end());
-  clean_vector.erase(std::unique(clean_vector.begin(), clean_vector.end()), clean_vector.end());
+  std::cout << "Quantidade de clientes únicos: " << client_count << std::endl;
+  std::cout << "Quantidade de produtos únicos: " << product_count << std::endl;
 }
 
 std::string PurchaseHistory::get_client_code_by_id(int client) {
